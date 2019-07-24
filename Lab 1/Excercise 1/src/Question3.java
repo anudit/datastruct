5f55a7bdb2c8d4ce7ac9784e829159def3097a70
Anudit Nagar <nagaranudit@gmail.com>
2019-07-24 15:34:35 +0530

    Lab 1

---
+++ Lab 1/Excercise 1/src/Question3.java
@@ -0,0 +1,32 @@
+
+public class Question3 {
+	public static void main(String[] args) {
+		int height = 7;
+	    for (int i=1;i<=height;i++){
+            int st_cnt = (2*i - 1);
+            int sp_cnt = ((2*height - 1) - st_cnt)/2;
+            while(sp_cnt != 0){
+                System.out.print(" ");
+                sp_cnt--;
+            }
+            while(st_cnt != 0){
+                System.out.print("*");
+                st_cnt--;
+            }
+	        System.out.print("\n");
+	    }
+	    for (int i=height-1;i>=1;i--){
+	    	int st_cnt = (2*i - 1);
+            int sp_cnt = ((2*height - 1) - st_cnt)/2;
+            while(sp_cnt != 0){
+                System.out.print(" ");
+                sp_cnt--;
+            }
+            while(st_cnt != 0){
+                System.out.print("*");
+                st_cnt--;
+            }
+            System.out.print("\n");
+	    }
+	}
+}
